7991e2df0d7af7b1aec05c7558c198c8125ac453
victorcondack1 <victorcondack@gmail.com>
2020-01-26 17:09:13 -0300

    alterados valores

--- src/entities/product.java
+++ src/entities/product.java
@@ -1,11 +1,22 @@
 package entities;
 
-public class product {
+public class Product {
 
  public String name;
  public double price;
  public int quantity;
  
+ public product(String name, double price, int quantity) {
+	 this.name = name;
+	 this.price = price;
+	 this.quantity = quantity;
+ }
+ 
+ public product(String name, double price, int quantity) {
+	 this.name = name;
+	 this.price = price;
+	 quantity = 0;
+ }
  public double totalValueInStock() {
 	 return price * quantity;
 	 
